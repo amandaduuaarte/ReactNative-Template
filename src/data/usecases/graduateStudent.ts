250f4cb9a32f2953a53461658d4d2956e4e3d280
Amanda Duarte <amandaduuaarte@gmail.com>
2024-01-30 20:47:05 -0300

    :sparkles: feat(settings): create usecase for storage functions

---
+++ src/data/usecases/graduateStudent.ts
@@ -0,0 +1,42 @@
+import {GraduatedStudentModel} from '../../domain/models';
+import {IGraduatedStudent} from '../../domain/usecases';
+import {IHttpClient} from '../protocols';
+import {IStorage} from '../protocols/storage/storage';
+
+export class GraduatedStudent implements IGraduatedStudent {
+  constructor(
+    private readonly HttpClient: IHttpClient<GraduatedStudentModel.Response>,
+    private readonly url: string,
+    private readonly Storage: IStorage<GraduatedStudentModel.Response>,
+  ) {}
+
+  async isGraduated(params: GraduatedStudentModel.Params) {
+    try {
+      const response = await this.HttpClient.request({
+        method: 'GET',
+        url: this.url,
+        body: params,
+      });
+
+      if (response.body) {
+        this.save(response.body, 'student');
+      }
+      return response.body;
+    } catch (err: any) {
+      console.error(err.message);
+      return;
+    }
+  }
+
+  async save(params: GraduatedStudentModel.Response, storageKey: string) {
+    // MMKV implementation
+    this.Storage.setItem(storageKey, params);
+    console.log(params);
+  }
+
+  get(storageKey: string) {
+    // MMKV implementation
+    const content = this.Storage.getItem(storageKey);
+    return content;
+  }
+}
--- src/domain/usecases/graduatedStudent.ts
+++ src/domain/usecases/graduatedStudent.ts
@@ -3,5 +3,13 @@ import {GraduatedStudentModel} from '../models';
 export interface IGraduatedStudent {
   isGraduated: (
     params: GraduatedStudentModel.Params,
-  ) => Promise<GraduatedStudentModel.Response>;
+  ) => Promise<GraduatedStudentModel.Response | undefined>;
+
+  // Salvar os estudantes que vão se graduar na semana (salvar no storage)
+  save: (
+    params: GraduatedStudentModel.Response,
+    storageKey: string,
+  ) => Promise<void>;
+
+  get: (storageKey: string) => GraduatedStudentModel.Response;
 }
