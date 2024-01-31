8adc1ea7b88df0051a40ac33e1a3b658ffcd6a25
Amanda Duarte <amandaduuaarte@gmail.com>
2024-01-30 21:03:33 -0300

    :sparkles: feat(settings): implementing MMKV structure

---
+++ src/infra/storage/mmkvStorage.ts
@@ -0,0 +1,20 @@
+import {MMKV} from 'react-native-mmkv';
+import {IStorage} from '../../data/protocols/storage/storage';
+
+export class MMKVStorage<T> implements IStorage<T> {
+  constructor(private readonly mmkv: MMKV) {
+    this.mmkv = new MMKV();
+  }
+  setItem(key: string, value: T): void {
+    this.mmkv.set(key, JSON.stringify(value));
+  }
+
+  deleteItem(key: string): void {
+    this.mmkv.delete(key);
+  }
+
+  getItem(key: string): T {
+    const content = this.mmkv.getString(key) || '';
+    return JSON.parse(content);
+  }
+}
