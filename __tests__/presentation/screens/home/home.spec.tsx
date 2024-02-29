49f3cd9b190802ca262e2b790a2f60cc9e3cd922
Amanda Duarte <amandaduuaarte@gmail.com>
2024-02-28 22:40:03 -0300

    :white_check_mark: test(tests): adjusting tests for save method

--- __tests__/presentation/screens/home/home.spec.tsx
+++ __tests__/presentation/screens/home/home.spec.tsx
@@ -1,6 +1,6 @@
 import {act, fireEvent, render} from '@testing-library/react-native';
 
-import * as Save from '@/main/factories/graduatedStudent/graduatedStudent-factory';
+import {GraduatedStudentFactory} from '@/main/factories/graduatedStudent/graduatedStudent-factory';
 import {Home} from '@/presentation/screens';
 
 const sut = () => {
@@ -15,7 +15,8 @@ describe('Home Screen', () => {
 
   it('should save data in storage when button was pressed', () => {
     const {getByTestId} = sut();
-    const saveMock = jest.spyOn(Save, 'saveFactory');
+
+    const saveMock = jest.spyOn(GraduatedStudentFactory, 'save');
 
     const button = getByTestId('save_button');
     act(() => {
